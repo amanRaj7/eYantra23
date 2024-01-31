aba5bfee298e6b94ceaaf8fd9c16982f9f2fc9bb
Aman Raj <92115066+amanRaj7@users.noreply.github.com>
2024-01-31 17:36:39 +0530

    Update led.py

--- task_4/led.py
+++ task_4/led.py
@@ -24,19 +24,33 @@ import argparse
 # * Example Call: distance((1,2), (3,4))
 '''
 def distance(point1, point2):
-    return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) 
+    return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
-# Construct the argument parser and parse the arguments
-parser = argparse.ArgumentParser(prog='led_detection', description="process local images") # Create Argument Parser object
-parser.add_argument('--image', type=str) # Add the image argument to the parser
-args = parser.parse_args() # Parse the command-line arguments
-image_name = args.image    # Access the value of the image argument
-script_dir = os.path.dirname(__file__) # Get the path of the directory where this script is located
-image_path = os.path.join(script_dir, image_name) # Create the full path of the image file
 
-print("Image Path:", image_path) # Print the image path(debugging)
+'''
+# * Function Name: load_image
+# * Input: NONE
+# * Output: image_name, image_path
+# * Logic: Loads the image from the local directory
+# * Example Call: load_image()
+'''
+def load_image():
+    parser = argparse.ArgumentParser(prog='led_detection', description="process local images") # Create Argument Parser object
+    parser.add_argument('--image', type=str) # Add the image argument to the parser
+    args = parser.parse_args() 
+    image_name = args.image    # Access the value of the image argument
+    # script_dir = os.path.dirname(__file__) # Get the path of the directory where this script is located
+    # image_path = os.path.join(script_dir, image_name) # Create the full path of the image file
+    image_path = image_name
+    return image_name, image_path
+image_name, image_path = load_image() # Load the image
 
-# Dictionary to store the number of LEDs in each organism
+
+image = cv.imread(image_path, 1) # Read the image
+# print("Image Path:", image_path) # Print the image path(debugging)
+
+
+''' Dictionary to store the number of LEDs in each organism '''
 alien_names = {
     2: 'alien_a',
     3: 'alien_b',
@@ -44,129 +58,159 @@ alien_names = {
     5: 'alien_d',
 }
 
-# load the image, 
-image = cv.imread(image_path, 1)
+'''
+# * Function Name: image_processing
+# * Input: image
+# * Output: thresh
+# * Logic: Preprocesses the image
+# * Example Call: image_processing(image)
+'''
+def image_processing(image):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # convert the image to grayscale
+    blurred = cv.GaussianBlur(gray, (7, 7), 0) # apply a Gaussian blur with a 7x7 kernel to the grayscale image
+    thresh = cv.threshold(blurred, 200, 255, cv.THRESH_BINARY)[1] # threshold the image to reveal light regions in the blurred image
 
-# convert it to grayscale, and blur it
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # perform a series of erosions and dilations to remove any small blobs of noise from the thresholded image
+    thresh = cv.erode(thresh, None, iterations=2)
+    thresh = cv.dilate(thresh, None, iterations=2)
+    return thresh
+thresh = image_processing(image) # Preprocess the image
+# cv.imshow("Image", thresh) # Display the image(For debugging)
+# cv.waitKey(1000) # Wait for 1 second(For debugging)
 
-# apply a Gaussian blur with a 9X9 kernel to the image to smooth it, useful when reducing high frequency noise
-blurred = cv.GaussianBlur(gray, (9, 9), 0)
+'''
+# * Function Name: image_masking
+# * Input: thresh
+# * Output: mask
+# * Logic: Masks the image
+# * Example Call: image_masking(thresh)
+'''
+def image_masking(thresh):
+    labels = measure.label(thresh, background=0) # label the connected components in the image
+    mask = np.zeros(thresh.shape, dtype="uint8") # initialize the mask to store only the "large" components
 
-# threshold the image to reveal light regions in the blurred images
-thresh = cv.threshold(blurred, 200, 255, cv.THRESH_BINARY)[1]
+    # loop over the unique components
+    for label in np.unique(labels):
+        # if this is the background label, ignore it
+        if label == 0:
+            continue
 
-# perform a series of erosions and dilations to remove any small blobs of noise from the thresholded image
-thresh = cv.erode(thresh, None, iterations=2)
-thresh = cv.dilate(thresh, None, iterations=2)
+        # otherwise, construct the label mask and count the number of pixels 
+        labelMask = np.zeros(thresh.shape, dtype="uint8")
+        labelMask[labels == label] = 255
+        numPixels = cv.countNonZero(labelMask)
+        #print(numPixels) # Print the number of pixels in each component (debugging)
 
-# perform a connected component analysis on the thresholded image, then initialize a mask to store only the "large" components
-labels = measure.label(thresh, background=0)
-mask = np.zeros(thresh.shape, dtype="uint8")
+        # if the number of pixels in the component is sufficiently large, then add it to our mask of "large blobs"
+        if numPixels > 20: 
+            mask = cv.add(mask, labelMask)
+    return mask
+mask = image_masking(thresh) # Mask the image
+contours, _ = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # find the contours in the mask
+contours = sorted(contours, key=lambda c: cv.minEnclosingCircle(c)[0]) # Sort the contours from left to right
 
-# loop over the unique components
-for label in np.unique(labels):
-    # if this is the background label, ignore it
-    if label == 0:
-        continue
-
-	# otherwise, construct the label mask and count the number of pixels 
-    labelMask = np.zeros(thresh.shape, dtype="uint8")
-    labelMask[labels == label] = 255
-    numPixels = cv.countNonZero(labelMask)
-    #print(numPixels) # Print the number of pixels in each component (debugging)
-
-	# if the number of pixels in the component is sufficiently large, then add it to our mask of "large blobs"
-    if numPixels > 20: 
-        mask = cv.add(mask, labelMask)
-
-# find the contours in the mask, then sort them from left to right
-contours, _ = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-# loop over the contours
-contours = sorted(contours, key=lambda c: cv.minEnclosingCircle(c)[0])
-
-# Initialize lists to store centroid coordinates and area
+''' Initialize lists to store centroid coordinates and area '''
 centroid = []
 area = []
+# debug = []
 
-# Loop over the contours
-for i, contour in enumerate(contours):
-    # Calculate the area of the contour
-    areas = cv.contourArea(contour)
-    M = cv.moments(contour)
-    c_X = round(M["m10"] / M["m00"],10)
-    c_Y = round(M["m01"] / M["m00"],10)
-    cX = int(c_X)
-    cY = int(c_Y)
+'''
+# * Function Name: centroid_calculation
+# * Input: contours
+# * Output: led_clusters, clusters
+# * Logic: Calculates the centroid coordinates and area of each LED
+# * Example Call: centroid_calculation(contours)
+'''
+def centroid_calculation(contours):
+    ''' Loop over the contours '''
+    for i, contour in enumerate(contours):
+        #Calculate the area of the contour
+        areas = cv.contourArea(contour)
+        M = cv.moments(contour)
+        c_X, c_Y = round(M["m10"] / M["m00"],10), round(M["m01"] / M["m00"],10)
+        cX, cY = int(c_X), int(c_Y)
 
-    # Draw the bright spot on the image
-    cv.drawContours(image, [contour], -1, (0, 0, 255), 3)
-    cv.putText(image, f'#{i + 1}', (cX, cY - 18), cv.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
+        # put the centroid coordinates in the list
+        # debug.append((cX, cY))
+        centroid.append((c_X, c_Y))
+        area.append(areas)
+        
+        # Draw the bright spot on the image (debugging)
+        cv.drawContours(image, [contour], -1, (0, 0, 255), 3)
+        cv.putText(image, f'#{i + 1}', (cX, cY - 18), cv.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
 
-    # Append centroid coordinates and area to the respective lists
-    centroid.append((c_X, c_Y))
-    area.append(areas)
+    ''' Assign values to the variables '''
+    length_factor = 12 if(sum(area)/len(area) > 900) else 9 # Calculate the length factor based on the maximum area ----> distance from ground
+    radius = int(np.sqrt(max(area) / np.pi)) # Calculate the radius of the bounding circle
+    led_coordinates = centroid # Store the centroid coordinates in a new list
+    a = len(centroid) # a is the number of LEDs
+    threshold_distance = radius*length_factor # Set a threshold distance to consider LEDs in the same cluster
+    led_clusters = [] # Initialize a list to store the clusters
 
-# Calculate the radius and length of the bounding box
-radius = int(np.sqrt(max(area) / np.pi))
-length = radius*8
+    # Draw the bounding circle and centroid on the image (debugging)
+    # for i in range(len(centroid)):
+        # cv.circle(image, debug[i], threshold_distance, (0, 255, 0), 3)
 
-# Draw the bounding box and centroid on the image
-led_coordinates = centroid
-a = len(centroid)
+    ''' Iterate through LED coordinates to identify clusters '''
+    for led in led_coordinates:
+        added_to_cluster = False # Check if the LED can be added to an existing cluster
+        for cluster in led_clusters:
+            if any(distance(led, cluster_led) < threshold_distance for cluster_led in cluster):
+                cluster.append(led)
+                added_to_cluster = True
+                break
 
-threshold_distance = radius*8.8   # Set a threshold distance to consider LEDs in the same cluster
+        # If the LED couldn't be added to any existing cluster, create a new cluster
+        if not added_to_cluster:
+            led_clusters.append([led])
 
-led_clusters = []  # Initialize a list to store LED clusters
+    ''' Calculate the centroid coordinates and area of each cluster'''
+    k = len(led_clusters) # k is the number of clusters
+    clusters = [] # Initialize a list to store the cluster centers
+    for i in range(k):
+        g = len(led_clusters[i])  # g is the number of leds in the cluster=
+        x, y = 0, 0 # Initialize x and y coordinates
+        for j in range(g):
+            x += led_clusters[i][j][0]/g
+            y += led_clusters[i][j][1]/g
+    
 
-# Iterate through LED coordinates to identify clusters
-for led in led_coordinates:
+        cluster_center = (round(x,4), round(y,4)) # Calculate the centroid of the cluster
+        clusters.append(cluster_center) # Store the centroid coordinates in a new list
 
-    added_to_cluster = False # Check if the LED can be added to an existing cluster
-    for cluster in led_clusters:
-        if any(distance(led, cluster_led) < threshold_distance for cluster_led in cluster):
-            cluster.append(led)
-            added_to_cluster = True
-            break
 
-    # If the LED couldn't be added to any existing cluster, create a new cluster
-    if not added_to_cluster:
-        led_clusters.append([led])
+        cv.circle(image, (int(x),int(y)), threshold_distance, (0, 0, 255), 3) # Draw the bounding circle on the image (debugging)
+        cv.putText(image, f'x', (int(x), int(y)), cv.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # Draw the centroid on the image (debugging)
 
-# Draw the bounding box and centroid on the image
-k = len(led_clusters)
-clusters = []
-for i in range(k):
-    g = len(led_clusters[i])  # g is the number of leds in the cluster
-    # x,y of centroid of cluster by averaging up all the x,y coordinates of the leds
-    x=0
-    y=0
-    for j in range(g):
-        x += led_clusters[i][j][0]/g
-        y += led_clusters[i][j][1]/g
-   
-    cluster_center = (round(x,4), round(y,4))
-    clusters.append(cluster_center)
-    cv.circle(image, (int(x),int(y)), length, (0, 0, 255), 3)
+    return led_clusters, clusters
+led_clusters, clusters = centroid_calculation(contours) # Calculate the centroid coordinates and area of each LED
 
-cv.imshow("Image", image) # Display the image(For debugging)
-cv.waitKey(0)
-# Save the output image as a PNG file
-cv.imwrite("led_detection_results.png", image)
+'''
+# * Function Name: output_file
+# * Input: NONE
+# * Output: NONE
+# * Logic: Saves the output file
+# * Example Call: output_file()
+'''
+def output_file():
+    # cv.imshow("Image", image) # Display the image(For debugging)
+    # cv.waitKey(1000) # Wait for 1 second(For debugging)
 
-filename = image_name.replace(".png", "") # Get the filename without the extension
+    filename = image_path.replace(".png", "") # Get the filename without the extension
+    # filename = filename.replace(".jpg", "") # Get the filename without the extension (For our convenience)
+    # filename = filename.replace(".jpeg", "") # Get the filename without the extension (For our convenience)
 
-# Open a text file for writing
-with open(f"{filename}.txt", "w") as file:
-    # Loop over the contours
-    for i, clusters in enumerate(clusters):
-        # Write centroid coordinates and area for each LED to the file
-        g= len(led_clusters[i])
-        file.write(f"Organism Type: {alien_names[g]}\n")
-        print(f"Organism Type: {alien_names[g]}\n") # Print the type of organisms (debugging)
-        file.write(f"Centroid: {clusters}\n\n")
-        print(f"Centroid: {clusters}\n\n") # Print the centroid (debugging)
-# Close the text file
-file.close()
+    cv.imwrite(f"{filename}_output.png", image) # Save the image
 
+    ''' Open a text file for writing '''
+    with open(f"{filename}.txt", "w") as file:
+
+        # Write the number of LEDs in each cluster to the file
+        for i, cluster in enumerate(clusters):
+            g= len(led_clusters[i]) # g is the number of LEDs in the cluster
+            file.write(f"Organism Type: {alien_names[g]}\n") # Write the type of organisms to the file
+            file.write(f"Centroid: {cluster}\n\n") # Write the centroid coordinates to the file
+
+            # print(f"Organism Type: {alien_names[g]}\n") # Print the type of organisms (debugging)
+            # print(f"Centroid: {cluster}\n\n") # Print the centroid (debugging)
+output_file() # Save the output file
